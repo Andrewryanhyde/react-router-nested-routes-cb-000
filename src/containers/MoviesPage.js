b241475dea35069469700d20488f035c35a11377
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-11-09 15:44:06 +0000

    automatically backed up by learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import MoviesList from '../components/MoviesList';
- 
-const MoviesPage = ({ movies }) => 
+
+const MoviesPage = ({ movies }) =>
   <div>
     <MoviesList movies={movies} />
   </div>;
- 
+
 const mapStateToProps = (state) => {
   return {
     movies: state.movies
   };
 }
- 
+
 export default connect(mapStateToProps)(MoviesPage);
